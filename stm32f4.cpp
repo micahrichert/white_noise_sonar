4cd98e6154e46b16b454ea35cbd83be252abc091
Micah Richert <richert@braincorporation.com>
2016-05-09 16:19:44 -0700

    testing converting analog to binary

--- stm32f4.cpp
+++ stm32f4.cpp
@@ -1,7 +1,6 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdarg.h>
-#include <algorithm>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/dma.h>
@@ -9,39 +8,42 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/f4/rng.h>
+#include <libopencm3/stm32/adc.h>
 #include <stdio.h>
 
-#define NR_OUTPUTS 2
-#define NR_INPUTS 3
-#define MAX_DISTANCE 1.5//5.0 // m
-#define TIME_LAG 0.0000 // s, time to shift xcor analysis by to compensate for speaker lag
-#define XCOR_DECAY_TIME 1.000 // s
-#define SIGMA 1.0 // how many standard deviations greater than chance should be considered significant
-#define MAX_SAMPLE_RATE 75000 // Hz //3in, 2out at 1.5m
-//#define MAX_SAMPLE_RATE 88000 // Hz //2in, 2out at 1.5m
-//#define MAX_SAMPLE_RATE 111500 // Hz //1in, 2out at 1.5m
-//#define MAX_SAMPLE_RATE 89500 // Hz //4in, 1out at 1.5m
-//#define MAX_SAMPLE_RATE 96500 // Hz //3in, 1out at 1.5m
-//#define MAX_SAMPLE_RATE 121000 // Hz //2in, 1out at 1.5m
-//#define MAX_SAMPLE_RATE 152000 // Hz //1in, 1out at 1.5m
+const int NOISE_LEVEL = 0; // add hysteresis to input when using analog/ADC
+const int USE_ANALOG_INPUT = 1;
+const int NR_OUTPUTS = 2;
+const int NR_INPUTS = 3;
+const float MAX_DISTANCE = 1.5;//5.0 // m
+const float TIME_LAG = 0.0000; // s, time to shift xcor analysis by to compensate for speaker lag
+const float XCOR_DECAY_TIME = 1.000; // s
+const float SIGMA = 1.0; // how many standard deviations greater than chance should be considered significant
+const float MAX_SAMPLE_RATE = 75000; // Hz //3in, 2out at 1.5m
+//const float MAX_SAMPLE_RATE = 88000; // Hz //2in, 2out at 1.5m
+//const float MAX_SAMPLE_RATE = 111500; // Hz //1in, 2out at 1.5m
+//const float MAX_SAMPLE_RATE = 89500; // Hz //4in, 1out at 1.5m
+//const float MAX_SAMPLE_RATE = 96500; // Hz //3in, 1out at 1.5m
+//const float MAX_SAMPLE_RATE = 121000; // Hz //2in, 1out at 1.5m
+//const float MAX_SAMPLE_RATE = 152000; // Hz //1in, 1out at 1.5m
 
-#define SPEED_OF_SOUND (340.3/2.0) // m/s, divide by 2 because sound travels out and back
-#define SAMPLE_RATE (MAX_SAMPLE_RATE)
-#define DISTANCE_PER_SAMPLE (SPEED_OF_SOUND/SAMPLE_RATE)
-#define NR_SAMPLES (int(MAX_DISTANCE/DISTANCE_PER_SAMPLE))
-#define XCOR_DECAY_SAMPLES (int(SAMPLE_RATE * XCOR_DECAY_TIME))
-#define XCOR_DECAY_RATE (1.0-1.0/XCOR_DECAY_SAMPLES)
-#define SIGMA2 (SIGMA*SIGMA) // the code works with variance instead of standard deviation
-#define SAMPLE_LAG (int(TIME_LAG*SAMPLE_RATE)+1) // add 1 because inputs are always sampled time step behind
-#define HISTORY_LEN (NR_SAMPLES/32+1)
+const float SPEED_OF_SOUND = 340.3/2.0; // m/s, divide by 2 because sound travels out and back
+const float SAMPLE_RATE = MAX_SAMPLE_RATE;
+const float DISTANCE_PER_SAMPLE = SPEED_OF_SOUND/SAMPLE_RATE;
+const int NR_SAMPLES = MAX_DISTANCE/DISTANCE_PER_SAMPLE;
+const int XCOR_DECAY_SAMPLES = SAMPLE_RATE * XCOR_DECAY_TIME;
+const float XCOR_DECAY_RATE = 1.0 - 1.0/XCOR_DECAY_SAMPLES;
+const float SIGMA2 = SIGMA*SIGMA; // the code works with variance instead of standard deviation
+const int SAMPLE_LAG = TIME_LAG*SAMPLE_RATE + 1; // add 1 because inputs are always sampled time step behind
+const int HISTORY_LEN = NR_SAMPLES/32 + 1;
 
-#define SERIAL_MESSAGE_BUFFER_SIZE (1024*5)
 
+// serial printf
+const int SERIAL_MESSAGE_BUFFER_SIZE = 1024*5;
 static char            tx_buffer[2][SERIAL_MESSAGE_BUFFER_SIZE];
 static uint32_t        num_chars_to_send = 0;
 static uint32_t        available_buffer  = 0;
 static volatile bool   transfer_complete = true;
-static char            printf_buffer[SERIAL_MESSAGE_BUFFER_SIZE];
 
 void dma1_stream3_isr()
 {
@@ -141,26 +143,27 @@ void serial_printf_helper(bool block, const char* pFormat, ...)
     va_list ap;
     uint32_t     num_written;
 
+    if (block) // if blocking
+    {
+        serial_printf_flush();
+        while (!transfer_complete);
+    }
+
     va_start(ap, pFormat);
-    num_written = vsnprintf(printf_buffer, SERIAL_MESSAGE_BUFFER_SIZE, pFormat, ap);
+    num_written = vsnprintf(tx_buffer[available_buffer] + num_chars_to_send, SERIAL_MESSAGE_BUFFER_SIZE - num_chars_to_send, pFormat, ap);
     va_end(ap);
+
     if (num_written > 0)
     {
         if (block) // if blocking
         {
-            serial_printf_flush();
-            while (!transfer_complete);
             for (size_t i = 0; i<num_written; ++i)
-                usart_send_blocking(USART3, (uint8_t)printf_buffer[i]);
+                usart_send_blocking(USART3, (uint8_t)(tx_buffer[available_buffer] + num_chars_to_send)[i]);
         } else {
             // copy data to available tx_buffer
-            uint32_t num_chars_available = SERIAL_MESSAGE_BUFFER_SIZE - num_chars_to_send;
-            
-            if (num_chars_available < num_written) serial_printf_flush();
-            
-            uint32_t num_chars_to_copy   = std::min(num_written, num_chars_available);
-            memcpy(tx_buffer[available_buffer] + num_chars_to_send, printf_buffer, num_chars_to_copy);
-            num_chars_to_send += num_chars_to_copy;
+            num_chars_to_send += num_written;
+
+            if (SERIAL_MESSAGE_BUFFER_SIZE - num_chars_to_send < 100) serial_printf_flush();
         }
     }
 }
@@ -211,6 +214,13 @@ uint32_t get_ms_from_start()
     return time_ms;
 }
 
+inline uint32_t pin_to_ADC(uint8_t apin)
+{
+    if (apin == 0) return ADC1;
+    else if (apin == 1) return ADC2;
+    return ADC3;
+}
+
 void setup()
 {
     clock_setup();
@@ -225,10 +235,34 @@ void setup()
     }
     
 	rcc_periph_clock_enable(RCC_GPIOA);
+    if (USE_ANALOG_INPUT)
+    {
+	    rcc_periph_clock_enable(RCC_ADC1);
+	    rcc_periph_clock_enable(RCC_ADC2);
+	    rcc_periph_clock_enable(RCC_ADC3);
+    }
 
 	for (uint8_t apin=0; apin<NR_INPUTS; apin++)
 	{
-        gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, 1<<apin); // Use PA0...
+	    if (USE_ANALOG_INPUT)
+	    {
+	        uint32_t adc = pin_to_ADC(apin);
+
+            gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, 1<<apin); // Use PA0...
+	
+	        adc_off(adc);
+
+	        adc_disable_external_trigger_regular(adc);
+	        adc_disable_scan_mode(adc);
+	        adc_disable_eoc_interrupt(adc);
+    	    adc_set_single_conversion_mode(adc);
+            adc_set_continuous_conversion_mode(adc);
+	        adc_set_sample_time_on_all_channels(adc, ADC_SMPR_SMP_3CYC); // need to play with this value and the prescaler
+            adc_set_clk_prescale(ADC_CCR_ADCPRE_BY8);  // prescaler defaults to 2 at startup
+            adc_power_on(adc);
+        } else {
+            gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, 1<<apin); // Use PA0...
+        }
     }
     
 	rcc_periph_clock_enable(RCC_RNG);
@@ -277,6 +311,7 @@ int main(void)
     #define XCOR(i, dpin, apin) xcors[i][apin][dpin]
     HIST_TYPE rand_hist[NR_OUTPUTS][HISTORY_LEN] = {0}; // need one extra slot due to the analog value being delayed by 1 cycle
     HIST_TYPE input[NR_INPUTS] = {0};
+    int previous_input[NR_INPUTS] = {0};
     int32_t t = 0;
     uint32_t start_time = 0;
     uint32_t rnd = 0;
@@ -285,11 +320,24 @@ int main(void)
     int hist_i, xcor_i;
     int decay_i = 0;
     int r;
+    bool adc_too_slow = false;
+    int tmp_in;
     
     for (uint32_t i=0; i<65536; i++) wordbits[i] = __builtin_popcount(i);
     
     setup();
-    
+
+    // select a2d input pin and start the conversion
+    for (uint8_t apin=0; apin<NR_INPUTS; apin++)
+    {
+        if (USE_ANALOG_INPUT)
+        {
+            uint32_t adc = pin_to_ADC(apin);
+            adc_set_regular_sequence(adc, 1, &apin);
+        	adc_start_conversion_regular(adc);
+        }
+    }
+
     while (true)
     {
         int start=time_ms;
@@ -343,12 +391,27 @@ int main(void)
         for (uint8_t apin=0; apin<NR_INPUTS; apin++)
         {
             input[apin] <<= 1;
-            input[apin] |= gpio_get(GPIOA, 1<<apin)>>apin;
+            if (USE_ANALOG_INPUT)
+            {
+        	    uint32_t adc = pin_to_ADC(apin);
+                while (!adc_eoc(adc)) ;//adc_too_slow = true;
+                
+                tmp_in = adc_read_regular(adc);
+//                if ((tmp_in - previous_input[apin] < NOISE_LEVEL) & (tmp_in - previous_input[apin] > -NOISE_LEVEL))
+//                    input[apin] |= (input[apin]&0x2)>>1;
+//                else
+                    input[apin] |= tmp_in > 1600;//previous_input[apin];//((1<<12)/2);//previous_input[apin];
+                previous_input[apin] = tmp_in;
+//if (apin == 1) serial_printf(false, "%d\r\n", tmp_in);                
+            } else {
+                input[apin] |= gpio_get(GPIOA, 1<<apin)>>apin;
+            }
         }
 
         t += 1;
         if (t >= XCOR_DECAY_SAMPLES)
         {
+//while(1);
             int time = get_ms_from_start();
             t = 0;
             serial_printf(false, "\033[2J\033[1;1H"); // clear terminal screen
@@ -367,7 +430,7 @@ int main(void)
                 }
             }
             serial_printf_flush();
-            serial_printf(false, "%d %d\r\n", time - start_time, get_ms_from_start() - time);
+            serial_printf(false, "%d %d %s\r\n", time - start_time, get_ms_from_start() - time, adc_too_slow?"adc too slow":"");
             start_time = get_ms_from_start();
             serial_printf_flush();
         }
